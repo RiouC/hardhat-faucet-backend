02d212d8f6037214c13ac4e4d9ec7b772552ac8a
RiouC <riou.christophe@protonmail.com>
2021-06-16 16:47:55 +0200

    [Token] we use totalSupply instead of initialSupply

--- contracts/Token.sol
+++ contracts/Token.sol
@@ -6,15 +6,12 @@ import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 
 contract Token is ERC20 {
     address private _owner;
-    uint256 private _initialSupply;
 
     /**
      * @dev Initializes the contract setting the deployer as the initial owner.
      */
     constructor(address owner_, uint256 initialSupply_) ERC20("Token", "KCA") {
         _owner = owner_;
-
-        _initialSupply = initialSupply_;
         _mint(owner_, initialSupply_ * 10 ** decimals());
     }
 
@@ -24,12 +21,4 @@ contract Token is ERC20 {
     function owner() public view returns (address) {
         return _owner;
     }
-
-    /**
-     * @dev Returns the initial supply of the ERC20
-     */
-    function initialSupply() public view returns (uint256) {
-        return _initialSupply;
-    }
 }
-
