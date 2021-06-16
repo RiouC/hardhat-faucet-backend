c97dd111c208a02a9efbf56201446806c5da2a67
RiouC <riou.christophe@protonmail.com>
2021-06-16 14:49:32 +0200

    renaming

--- hardhat.config.js
+++ hardhat.config.js
@@ -4,7 +4,7 @@ require('hardhat-docgen');
 
 require('dotenv').config();
 const INFURA_PROJECT_ID = process.env.INFURA_PROJECT_ID;
-const PRIVATE_KEY = process.env.PRIVATE_KEY;
+const INFURA_PROJECT_SECRET = process.env.INFURA_PROJECT_SECRET;
 
 /**
  * @type import('hardhat/config').HardhatUserConfig
@@ -14,23 +14,23 @@ module.exports = {
   networks: {
     mainnet: {
       url: `https://mainnet.infura.io/v3/${INFURA_PROJECT_ID}`,
-      accounts: [`0x${PRIVATE_KEY}`],
+      accounts: [`0x${INFURA_PROJECT_SECRET}`],
     },
     ropsten: {
       url: `https://ropsten.infura.io/v3/${INFURA_PROJECT_ID}`,
-      accounts: [`0x${PRIVATE_KEY}`],
+      accounts: [`0x${INFURA_PROJECT_SECRET}`],
     },
     kovan: {
       url: `https://kovan.infura.io/v3/${INFURA_PROJECT_ID}`,
-      accounts: [`0x${PRIVATE_KEY}`],
+      accounts: [`0x${INFURA_PROJECT_SECRET}`],
     },
     rinkeby: {
       url: `https://rinkeby.infura.io/v3/${INFURA_PROJECT_ID}`,
-      accounts: [`0x${PRIVATE_KEY}`],
+      accounts: [`0x${INFURA_PROJECT_SECRET}`],
     },
     goerli: {
       url: `https://goerli.infura.io/v3/${INFURA_PROJECT_ID}`,
-      accounts: [`0x${PRIVATE_KEY}`],
+      accounts: [`0x${INFURA_PROJECT_SECRET}`],
     },
   },
   docgen: {
