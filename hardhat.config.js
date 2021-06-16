c01015294253607dbb59bb306444153a156a3e83
RiouC <riou.christophe@protonmail.com>
2021-06-16 15:31:06 +0200

    [hardhat.config.js] INFURA_PROJECT_SECRET -> DEPLOYER_PRIVATE_KEY

--- hardhat.config.js
+++ hardhat.config.js
@@ -4,7 +4,7 @@ require('hardhat-docgen');
 
 require('dotenv').config();
 const INFURA_PROJECT_ID = process.env.INFURA_PROJECT_ID;
-const INFURA_PROJECT_SECRET = process.env.INFURA_PROJECT_SECRET;
+const DEPLOYER_PRIVATE_KEY = process.env.DEPLOYER_PRIVATE_KEY;
 
 /**
  * @type import('hardhat/config').HardhatUserConfig
@@ -14,23 +14,23 @@ module.exports = {
   networks: {
     mainnet: {
       url: `https://mainnet.infura.io/v3/${INFURA_PROJECT_ID}`,
-      accounts: [`0x${INFURA_PROJECT_SECRET}`],
+      accounts: [`0x${DEPLOYER_PRIVATE_KEY}`],
     },
     ropsten: {
       url: `https://ropsten.infura.io/v3/${INFURA_PROJECT_ID}`,
-      accounts: [`0x${INFURA_PROJECT_SECRET}`],
+      accounts: [`0x${DEPLOYER_PRIVATE_KEY}`],
     },
     kovan: {
       url: `https://kovan.infura.io/v3/${INFURA_PROJECT_ID}`,
-      accounts: [`0x${INFURA_PROJECT_SECRET}`],
+      accounts: [`0x${DEPLOYER_PRIVATE_KEY}`],
     },
     rinkeby: {
       url: `https://rinkeby.infura.io/v3/${INFURA_PROJECT_ID}`,
-      accounts: [`0x${INFURA_PROJECT_SECRET}`],
+      accounts: [`0x${DEPLOYER_PRIVATE_KEY}`],
     },
     goerli: {
       url: `https://goerli.infura.io/v3/${INFURA_PROJECT_ID}`,
-      accounts: [`0x${INFURA_PROJECT_SECRET}`],
+      accounts: [`0x${DEPLOYER_PRIVATE_KEY}`],
     },
   },
   docgen: {
