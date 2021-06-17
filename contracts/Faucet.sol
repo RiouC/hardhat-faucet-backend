a648e366507efa94dfe0057c5ca0f99bbe21ad50
RiouC <riou.christophe@protonmail.com>
2021-06-17 16:09:35 +0200

    [Faucet.sol] add TimeRemaining event, natspec

--- contracts/Faucet.sol
+++ contracts/Faucet.sol
@@ -8,32 +8,31 @@ import "./Token.sol";
  *@author Karim Derradji
  *@notice You can use this contract for a decentralized Faucet, yet it is not recomanded to do so.
  *@dev Functions are basic, the app is highly restricted for security issues.
- * */
+ */
 pragma solidity ^0.8.4;
 
 contract Faucet {
     /**
-     *@dev This consumation of Address library is optional, so you can buy tokenFaucet with fake eth
-     * */
+     * @dev This consumation of Address library is optional, so you can buy tokenFaucet with fake eth
+     */
     using Address for address payable;
 
     Token private _tokenInstance;
 
     /**
-     *@dev The rate with which token are sent
+     * @dev The rate with which token are sent
      * 10 ** 18 = 1 TOKEN
-     * */
+     */
     uint256 private _rate = 1 * 10**18;
 
     /**
      *@dev interval between each withdrawal of token sets to 3 days
-     * */
+     */
     uint256 private constant _INTERVAL = 3 days;
 
     /**
-     *@dev mapping to keep track of access control relying on time
+     * @dev mapping to keep track of access control relying on time
      * access is restricted on withdraw with :
-     *
      */
     mapping(address => uint256) private _lastAccess;
 
@@ -41,14 +40,18 @@ contract Faucet {
     event TimeRemaining(uint256 currentTimestamp, uint256 timestampLastAccess);
 
     /**
-     *@dev constructor for Faucet :
-     *@param tokenAddress_ : the address of the token we want to transfer to users
+     * @dev constructor for Faucet :
+     * @param tokenAddress_ : the address of the token we want to transfer to users
      */
     constructor(address tokenAddress_) {
         require(tokenAddress_ != address(0), "Faucet");
         _tokenInstance = Token(tokenAddress_);
     }
 
+    /**
+     * @dev function to request tokens from the faucet
+     * emit a TokenSent event
+     */
     function requestTokens() public {
         require(allowedToWithdraw(msg.sender) == true, "Faucet: you're not allowed to withdraw anymore");
 
@@ -59,6 +62,12 @@ contract Faucet {
         emit TokenSent(msg.sender, amount);
     }
 
+    /**
+     * @notice Check if address_ can claim tokens again
+     * @dev we emit a TimeRemaining event to calculate how much time
+     * the user has to wait before claiming token again
+     * @return true or false
+     */
     function allowedToWithdraw(address address_) public view returns (bool) {
         emit TimeRemaining(block.timestamp, _lastAccess[address_]);
         if (_lastAccess[address_] == 0) {
